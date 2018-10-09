3ae5d3772141d6477bb7bf9e5b5030cf4a932151
Igor Null <m1el.2027@gmail.com>
2018-10-09 03:44:57 +0300

    implemented path trimming, added opts struct

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,63 @@ use walkdir::{WalkDir};
 use same_file::{Handle, Key};
 use ::std::collections::{HashSet};
 
+struct Opts {
+    filter_dups: bool,
+    flamegraph: bool,
+    list_files: bool,
+    reverse: bool,
+    truncate: bool,
+}
+
+impl Opts {
+    fn new() -> Opts {
+        Opts {
+            filter_dups: false,
+            flamegraph: false,
+            list_files: false,
+            reverse: false,
+            truncate: false,
+        }
+    }
+}
+
 fn main() {
-    let mut paths = Vec::<String>::new();
+    let mut roots = Vec::<String>::new();
     let mut skip_arg = false;
-    let mut filter_dups = false;
-    let mut list_files = false;
-    let mut flamegraph = false;
-    let mut reverse = false;
+    let mut opts = Opts::new();
     for arg in ::std::env::args().skip(1) {
         if skip_arg || !arg.starts_with('-') {
-            paths.push(arg);
+            roots.push(arg);
             continue;
         }
         for c in arg.chars().skip(1) {
             match c {
-                'l' => { list_files = true; },
-                'd' => { filter_dups = true; },
-                'r' => { reverse = true; }
+                'd' => { opts.filter_dups = true; },
                 'f' => {
-                    flamegraph = true;
-                    list_files = true;
+                    opts.flamegraph = true;
+                    opts.list_files = true;
                 },
+                'l' => { opts.list_files = true; },
+                'r' => { opts.reverse = true; }
+                't' => { opts.truncate = true; }
                 '-' => { skip_arg = true; },
                 _ => {},
             }
         }
     }
 
-    if paths.is_empty() {
-        paths.push(".".to_string());
+    if roots.is_empty() {
+        roots.push(".".to_string());
     }
 
-    for path in paths {
+    for root in roots {
         let mut total = 0u64;
         let mut dir_set = HashSet::<Key>::new();
         let mut file_set = HashSet::<Key>::new();
-        let iter = WalkDir::new(path)
+        let iter = WalkDir::new(&root)
             .into_iter()
             .filter_entry(|entry| {
-                if !filter_dups { return true; }
+                if !opts.filter_dups { return true; }
                 Handle::from_path(entry.path()).ok()
                     .and_then(|h| h.as_key())
                     .map_or(true, |key| dir_set.insert(key))
@@ -53,7 +71,7 @@ fn main() {
                 let meta = entry.metadata().ok()?;
                 if !meta.is_file() { return None; }
                 let len = meta.len();
-                if filter_dups {
+                if opts.filter_dups {
                     let key = Handle::from_path(path).ok()?.as_key()?;
                     if !file_set.insert(key.clone()) { return None; }
                 }
@@ -61,20 +79,28 @@ fn main() {
             });
         for (path, len) in iter {
             total += len;
-            if list_files {
-                if flamegraph {
-                    let show = path.display().to_string()
-                        .replace(|c|c=='/'||c=='\\', ";");
-                    println!("{1} {0}", len, show);
-                } else if reverse {
-                    println!("{1} {0}", len, path.display());
+            if opts.list_files {
+                let disp;
+                let mut st;
+                if let Some(path_str) = path.to_str() {
+                    st = path_str;
                 } else {
-                    println!("{} {}", len, path.display());
+                    disp = path.display().to_string();
+                    st = &disp;
+                }
+                st = st.trim_left_matches(&root);
+                if opts.flamegraph {
+                    let show = st.replace(|c|c=='/'||c=='\\', ";");
+                    println!("{1} {0}", len, show);
+                } else if opts.reverse {
+                    println!("{1} {0}", len, st);
+                } else {
+                    println!("{} {}", len, st);
                 }
             }
         }
-        if !list_files {
-            println!("{} {}", path, total);
+        if !opts.list_files {
+            println!("{} {}", root, total);
         }
     }
 }
